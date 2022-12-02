a1263011adca26aec425c13be5682556c8ed32ab
Zaheer <zaheerdev2@gmail.com>
2022-12-02 11:17:47 +0500

    file upload

--- src/pages/media/image-file.jsx
+++ src/pages/media/image-file.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React from "react";
+import MultiImageUpload from "../../component/ui-components/fileUpload/FileUpload";
 
 const ImageFile = () => {
-    return (
-        <div>
-            <p>Upload images from your device. Hold [Ctrl] or [Shift] to select multiple files. Maximum resolution allowed for images is 50MP.</p>
-            <div className='dragDropSec'>
-                Drop Some Files Here or Click to Choose
-            </div>
-        </div>
-    );
+    const checkFiles = (e) => {
+          console.log('e :>> ', e);
+    }
+  return (
+    <div>
+      <p>
+        Upload images from your device. Hold [Ctrl] or [Shift] to select
+        multiple files. Maximum resolution allowed for images is 50MP.
+      </p>
+      {/* <div className="dragDropSec">
+        Drop Some Files Here or Click to Choose */}
+        <MultiImageUpload
+          maxSize={2}
+          accept={["image/jpeg", "image/jpg", "image/png"]}
+          defaultImages={[]}
+          maxFiles={3}
+          onChange={checkFiles}
+        />
+      {/* </div> */}
+    </div>
+  );
 };
 
 export default ImageFile;
